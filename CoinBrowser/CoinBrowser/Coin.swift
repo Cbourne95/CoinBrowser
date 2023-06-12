a76012ee0880fb304abcd607979dc8d35d8157dd
Chris Bourne <chrisbourne@Chriss-Mac-mini.local>
2023-06-12 13:11:22 -0500

    Your commit message

---
+++ CoinBrowser/CoinBrowser/Coin.swift
@@ -0,0 +1,20 @@
+//
+//  Coin.swift
+//  CoinBrowser
+//
+//  Created by Chris Bourne on 6/12/23.
+//
+
+import Foundation
+
+Class Coin {
+    
+    let id: String
+    let symbol: String
+    let name: String
+    
+   // fail-able initailizer
+    init?(dictionary: )
+}
+    
+    
